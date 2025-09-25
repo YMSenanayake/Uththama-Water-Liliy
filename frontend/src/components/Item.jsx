2a70e19f38f13ab5dafde056d12a88a139a0a0b4
YMSenanayake <yasirumadhusankasenanayake@gmail.com>
2025-09-25 10:47:45 +0530

    NArrivals create

--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -6,6 +6,12 @@ const Item = ({ product }) => {
     const [hovered, setHovered] = useState(false)
     const [size, setsize] = useState(product.sizes[0]) // default size (first in the array)
 
+    // colors to cycle through
+    const colors = ["#f2f2f2", "#f6f9f6", "#f6f8fe"]
+    // compute on index from product._id; parseInt fallback to 0 for safety
+    const bgcolor = colors[(parseInt(product._id?.slice(-4) || "0", 16)) %
+        colors.length]
+
     return (
         <div>
             <div className="overflow-hidden">
@@ -14,12 +20,32 @@ const Item = ({ product }) => {
                     onMouseEnter={() => setHovered(true)}
                     onMouseLeave={() => setHovered(false)}
                     className="flexCenter h-[182px] w-full transition-all duration-100 rounded-xl group relative"
-                // style={{backgroundColor: bgcolor}}
+                    style={{ backgroundColor: bgcolor }}
                 >
 
                     <img src={product.images.length > 1 && hovered ? product.images
-                    [1] : product.images[0]} alt="" height={144} width={144}/>
-
+                    [1] : product.images[0]} alt="" height={144} width={144} />
+                    <div className='absolute bottom-1 left-1 right-1 hidden group-hover:block'>
+                        <button
+                            onClick={() => {
+                                navigate(`/products/${product._id}`);
+                                scrollTo(0, 0);
+                            }}
+                            className = 'btn-secondary !py-2 !px-0 w-full !text-xs'
+                        >
+                            Quick View
+                        </button>
+                    </div>
+                    <p className='absolute top-2 right-2 ring-1 ring-slate-900/10 px-5 bg-white/50 rounded-full'>{product.type}</p>
+                </div>
+                {/* info */}
+                <div className="pt-3 p-1">
+                    {/* Title and description */}
+                    <div className='flexBetween'>
+                        <h5 className='h5 uppercase line-clamp-1'>{product.title}</h5>
+                        <p className='uppercase font-semibold'>{currency} {product.price[size]}.00</p>
+                    </div>
+                    <p className='line-clamp-2 pt-1'>{product.description}</p>
                 </div>
             </div>
         </div>
