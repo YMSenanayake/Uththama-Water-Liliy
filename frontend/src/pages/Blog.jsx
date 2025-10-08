a5174fe8b1313625ad7f96344d5a22529f1ebdfd
YMSenanayake <yasirumadhusankasenanayake@gmail.com>
2025-10-08 13:30:57 +0530

    createBlog

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { blogs } from '../assets/data'
 
 function Blog() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='bg-primary pt-28 pb-28'>
+            <div className='max-padd-containers'>
+                {/* container */}
+                <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 gap-y-12'>
+                    {blogs.map((blog, index) => (
+                        <div key={index} className='relative'>
+                            <div className='bg-secondary/10 p-4 rounded-2xl'>
+                                <img src={blogs.image} alt="" className='shadow-xl shadow-slate-900/20 rounded-xl' />
+                            </div>
+                            <p className='medium-14 mt-6'>{blogs.category}</p>
+                            <h5 className='h5 pr-4 mb-1 line-clamp-2'>{blogs.title}</h5>
+                            <p>{blog.description}</p>
+                            <button className='underline mt-2 bold-14 line-clamp-2'>
+                                continue reading
+                            </button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Blog
